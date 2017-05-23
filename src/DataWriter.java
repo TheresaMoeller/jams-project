dc108d778276b6b9116ac261cfd7bfb2c71c4f68
Theresa <Theresa@192.168.0.9>
2017-05-23 11:35:10 +0200

    File writer implementation

--- src/DataWriter.java
+++ src/DataWriter.java
@@ -1,75 +1,128 @@
+
 import jams.data.*;
 import jams.model.*;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Theresa
  */
-public class DataWriter extends JAMSComponent{
-    
+public class DataWriter extends JAMSComponent {
+
     @JAMSVarDescription(access = JAMSVarDescription.AccessType.WRITE,
             description = "windspeed value read from file")
     public static Attribute.FileName outfile;
-    
+
+    @JAMSVarDescription(access = JAMSVarDescription.AccessType.READ,
+            description = "Month")
+    public static Attribute.String date;
+
     @JAMSVarDescription(access = JAMSVarDescription.AccessType.READ,
             description = "Precip value read from file")
     public static Attribute.Double precip;
-    
+
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.READ, //gemessener Abfluss?
+            description = "Simulated runoff",
+            lowerBound = 0,
+            upperBound = Double.POSITIVE_INFINITY
+    )
+    public Attribute.Double simRunoff;
+
+    @JAMSVarDescription(access = JAMSVarDescription.AccessType.READ,
+            description = "Precip value read from file")
+    public static Attribute.Double snowStor;
+
+    // Attribut für Schneeschmelze
+    @JAMSVarDescription(access = JAMSVarDescription.AccessType.READ,
+            description = "Precip value read from file")
+    public static Attribute.Double baseStor;
+
+    @JAMSVarDescription(access = JAMSVarDescription.AccessType.READ, //aktuelle Verdunstung
+            description = "Precip value read from file")
+    public static Attribute.Double ET;
+
+    Map<String, List<Double>> num = new HashMap<String, List<Double>>();
+
     @Override
-    public void init() {  
-        
-        File file = new File(this.getModel().getWorkspaceDirectory(), outfile.getValue());
-        
-        //neue Datei erzeugen
-        try{
-            file.createNewFile();
-        } catch (IOException ex){
-            getModel().getRuntime().sendHalt("An error occured. Couldn't create file " + outfile.getValue() + ". Aborting.");
-        }
-        
-        BufferedWriter bw = null;
-        FileWriter fw = null;
-        
-        try {
-            fw = new FileWriter(file);
-            bw = new BufferedWriter(fw);
-            
-            bw.write("# Results of JAMS Runoff Model");
-            bw.newLine();
-            
-            for( Double k: output){
-                bw.write(k.toString());
-                bw.newLine();
-            }
-            
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-        try {
-            bw.close();
-            fw.close();
-        } catch (IOException ex) {
-            System.out.println("Could not close Writer");
-        }
-        
+    public void init() {
+
+        //Erstelle Liste aller notwendigen Werte
+        List<Double> values = new ArrayList<Double>();
+        values.add(precip.getValue());
+        values.add(simRunoff.getValue());
+        values.add(snowStor.getValue());
+        values.add(baseStor.getValue());
+        values.add(ET.getValue());
+
+        //Speichere Datum und Werte in Hashmap
+        num.put(date.toString(), values);
     }
 
     @Override
     public void run() {
+
+        File file = new File(this.getModel().getWorkspaceDirectory(), outfile.getValue());
+
+        //neue Datei erzeugen
+        try {
+            file.createNewFile();
+        } catch (IOException ex) {
+            getModel().getRuntime().sendHalt("An error occured. Couldn't create file " + outfile.getValue() + ". Aborting.");
+        }
+
+        BufferedWriter bw = null;
+        FileWriter fw = null;
+
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
+        LocalDateTime current = LocalDateTime.now();
+
+        try {
+            fw = new FileWriter(file);
+            bw = new BufferedWriter(fw);
+
+            //Schreibe Kopf der Outputdatei
+            bw.write("# Results of JAMS Runoff Model");
+            bw.newLine();
+            bw.write("#date: " + dtf.format(current));
+            bw.newLine();
+            bw.write("#date percip simRunoff runoff snowStor snowMelt baseStor potET realET");
+            bw.newLine();
+
+            //Iteration über alle Map-Elemente
+            for (Map.Entry<String, List<Double>> entry : num.entrySet()) {
+                bw.write(entry.getKey() + " " + entry.getValue());
+                bw.newLine();
+            }
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+        
+        try {
+            bw.close();
+            fw.close();
+        } catch (IOException ex) {
+            getModel().getRuntime().sendHalt("Could not close Writer.");
+        }
+
     }
 
     @Override
     public void cleanup() {
-    } 
-    
+    }
+
 }
