a5f555fa412c95aed850f002bc82717eda1e1ad2
Anna-LenaS <anna-lena.seith@uni-jena.de>
2017-06-06 14:32:52 +0200

    Add files via upload
    fertige Version Precipitation + Snowmelt

--- src/Precipitation.java
+++ src/Precipitation.java
@@ -30,15 +30,16 @@ public class Precipitation extends JAMSComponent {
             lowerBound = Double.NEGATIVE_INFINITY,                             
             upperBound = Double.POSITIVE_INFINITY                                  
     )
-            public Attribute.Double baseTemp;                 
+    public Attribute.Double baseTemp;                 
 
+    
     @JAMSVarDescription(
             access = JAMSVarDescription.AccessType.READ,
             description = "Transition temperature zone between snow and rain",
             lowerBound = Double.NEGATIVE_INFINITY,
             upperBound = Double.POSITIVE_INFINITY
     )
-            public Attribute.Double transTemp;
+    public Attribute.Double transTemp;
     
   
     @JAMSVarDescription(
@@ -73,44 +74,45 @@ public class Precipitation extends JAMSComponent {
     
     @JAMSVarDescription(
         access = JAMSVarDescription.AccessType.READ,
-        description = "Filling of the soil storage at beginning of model run",
+        description = "Saturation value of SoilStorage. If value is exceeded, "
+                       + "the water flows into the depression storage",
         lowerBound = 0,
         upperBound = Double.POSITIVE_INFINITY,
         unit = "mm"
-        )
-        public Attribute.Double initSoilStor; 
- 
-        
-    @JAMSVarDescription(
-        access = JAMSVarDescription.AccessType.READ,
-        description = "Filling of the snow storage at beginning of model run",
-        lowerBound = 0,
-        upperBound = Double.POSITIVE_INFINITY,
-        unit = "mm"
-        )
-        public Attribute.Double initSnowStor;  
-    
-       
-    @JAMSVarDescription(
-        access = JAMSVarDescription.AccessType.READ,
-        description = "Filling of the depression storage at beginning of model run",
-        lowerBound = 0,
-        upperBound = Double.POSITIVE_INFINITY,
-        unit = "mm"
-        )
-        public Attribute.Double initDepStor;  
-    
-    @JAMSVarDescription(
-        access = JAMSVarDescription.AccessType.READ,
-        description = "Saturation value of SoilStorage. If value is exceeded, the "
-                        + "water flows into the depression storage",
-        lowerBound = 0,
-        upperBound = Double.POSITIVE_INFINITY,
-        unit = "mm"
-        )
-        public Attribute.Double satValue;   // !//Kalibrationsfaktor
+    )
+    public Attribute.Double satValue;  
      
     
+    @JAMSVarDescription(
+        access = JAMSVarDescription.AccessType.READWRITE,
+        description = "Filling of the soil storage after time step",
+        lowerBound = 0,
+        upperBound = Double.POSITIVE_INFINITY,
+        unit = "mm"
+    )
+    public Attribute.Double soilStor; 
+    
+    
+    @JAMSVarDescription(
+        access = JAMSVarDescription.AccessType.READWRITE,
+        description = "Filling of the snow storage after time step",
+        lowerBound = 0,
+        upperBound = Double.POSITIVE_INFINITY,
+        unit = "mm"
+    )
+    public Attribute.Double snowStor;  
+   
+    
+    @JAMSVarDescription(
+        access = JAMSVarDescription.AccessType.READWRITE,
+        description = "Filling of the depression storage after time step",
+        lowerBound = 0,
+        upperBound = Double.POSITIVE_INFINITY,
+        unit = "mm"
+    )
+    public Attribute.Double depStor; 
+    
+       
     @JAMSVarDescription(
             access = JAMSVarDescription.AccessType.WRITE,
             description = "Precipitation as snow",
@@ -130,107 +132,37 @@ public class Precipitation extends JAMSComponent {
     )
     public Attribute.Double precip_rain;
     
-    
-    @JAMSVarDescription(
-        access = JAMSVarDescription.AccessType.WRITE,
-        description = "Filling of the soil storage after time step",
-        lowerBound = 0,
-        upperBound = Double.POSITIVE_INFINITY,
-        unit = "mm"
-        )
-        public Attribute.Double soilStor; 
-    
-    
-    @JAMSVarDescription(
-        access = JAMSVarDescription.AccessType.WRITE,
-        description = "Filling of the snow storage after time step",
-        lowerBound = 0,
-        upperBound = Double.POSITIVE_INFINITY,
-        unit = "mm"
-    )
-    public Attribute.Double snowStor;  
-   
-    
-    
-    @JAMSVarDescription(
-        access = JAMSVarDescription.AccessType.WRITE,
-        description = "Filling of the depression storage after time step",
-        lowerBound = 0,
-        upperBound = Double.POSITIVE_INFINITY,
-        unit = "mm"
-        )
-        public Attribute.Double depStor; 
 
-    
     /*
      *  Component run stages
      */
     
     @Override
     public void init() {
-        
-//        //Check if needed values are existing + set initial storages before first model run
-//        if (this.initSoilStor == null)   
-//            getModel().getRuntime().sendHalt(JAMS.i18n("parameter_Precipitation_initSoilStor_unspecified"));
-//        else
-//            soilStor.setValue(initSoilStor.getValue());
-//        
-//        if (this.initSnowStor == null)  
-//            getModel().getRuntime().sendHalt(JAMS.i18n("parameter_Precipitation_initSnowStor_unspecified"));
-//        else
-//            snowStor.setValue(initSnowStor.getValue());
-//        
-//        if (this.initDepStor == null)   
-//            getModel().getRuntime().sendHalt(JAMS.i18n("parameter_Precipitation_initDepStor_unspecified"));
-//        else
-//            depStor.setValue(initDepStor.getValue());
-//        
-//        if (this.satValue == null)  
-//            getModel().getRuntime().sendHalt(JAMS.i18n("parameter_Precipitation_satValue_unspecified"));
     }
 
     @Override
     public void run() {
-        
-        // if there are no values for a time step
-//        if (this.precip_rain == null)  ?//raus
-//            getModel().getRuntime().sendHalt(JAMS.i18n("input_data_Precipitation_precip_rain_unspecified"));
-//        if (this.precip_snow == null)   ?//raus?
-//            getModel().getRuntime().sendHalt(JAMS.i18n("input_data_Precipitation_precip_snow_unspecified"));
-//        
-//        
-        // Take given values
-            
-        double bT = this.baseTemp.getValue(); //oder baseTemp.getValue()
-        double tT = this.transTemp.getValue();
-        double minT = this.mintemp.getValue();
-        double meanT = this.meantemp.getValue();
-        double p = this.precip.getValue(); 
-        double nightT = 0.5 * (meanT + minT); // Night temperature (mean of meanT and minT)
+                
+        double nighttemp = 0.5 * (meantemp.getValue() + mintemp.getValue()); // Night temperature (mean of meantemp and mintemp)
         double proportion;
         double snow_amount; 
         double rain_amount; 
-        double soilStor = this.soilStor.getValue(); 
-        double snowStor = this.snowStor.getValue(); 
-        double depStor = this.depStor.getValue();   
-        //double snow = this.precip_snow.getValue();  ?//raus?! siehe Calculation snowStor
-        //double rain = this.precip_rain.getValue();  ?//raus?! siehe Calculation soilStor
-        double sat = this.satValue.getValue(); 
-        
+               
          
-        if (p > 0) { //precipitation occurs
-            if (nightT > (bT + tT)) { // night temperature too high for snowfall
-            rain_amount = p;   
+        if (precip.getValue() > 0) { //precipitation occurs
+            if (nighttemp > (baseTemp.getValue() + transTemp.getValue())) { // night temperature too high for snowfall
+            rain_amount = precip.getValue();   
             snow_amount = 0;
             }
-            else if (nightT < (bT - tT)){ // night temperature too low for rainfall
+            else if (nighttemp < (baseTemp.getValue() - transTemp.getValue())){ // night temperature too low for rainfall
             rain_amount = 0;
-            snow_amount = p;
+            snow_amount = precip.getValue();
             }
             else{
-            proportion = (100/(tT * 2 )) * (nightT - bT) + 50; //water proportion in percentage oder: 25*nightT + 50
-            rain_amount = p * (proportion/ 100); 
-            snow_amount = p - rain_amount; 
+            proportion = (100/(transTemp.getValue() * 2 )) * (nighttemp - baseTemp.getValue()) + 50; //water proportion in percentage 
+            rain_amount = precip.getValue() * (proportion/ 100); 
+            snow_amount = precip.getValue() - rain_amount; 
             }
  
         } else { // no precipitation occurs
@@ -239,44 +171,28 @@ public class Precipitation extends JAMSComponent {
         }
      
         // Calculation of snowStor 
-        snowStor = snowStor + snow_amount; 
+        snowStor.setValue(snowStor.getValue() + snow_amount); 
         
         // Calculation of depStor and soilStor
-        if (soilStor < sat){ //if soilStor is unsaturated
-            soilStor = soilStor + rain_amount; //oder soilStor + rain
-            if (soilStor >= sat){  //if soilStor is saturated afterwards 
-                depStor = depStor + (soilStor - sat); //excess water flows into depStor
-                soilStor = sat; //soilStor gets saturation value
+        if (soilStor.getValue() < satValue.getValue()){ //if soilStor is unsaturated
+            soilStor.setValue(soilStor.getValue() + rain_amount); 
+            if (soilStor.getValue() >= satValue.getValue()){  //if soilStor is saturated afterwards 
+                depStor.setValue(depStor.getValue() + (soilStor.getValue() - satValue.getValue())); //excess water flows into depStor
+                soilStor.setValue(satValue.getValue()); //soilStor gets saturation value
             }            
         }
         else { //if soilStor is already saturated 
-            depStor = depStor + rain_amount;   //oder depStor + rain
-            soilStor = sat;  //soilStor gets saturation value
+            depStor.setValue(depStor.getValue() + rain_amount);   
+            soilStor.setValue(satValue.getValue());  //soilStor gets saturation value
         }
-         
-               
+                       
         //Write obtained values
         precip_rain.setValue(rain_amount);  
         precip_snow.setValue(snow_amount);
-        this.soilStor.setValue(soilStor); 
-        this.snowStor.setValue(snowStor); 
-        this.depStor.setValue(depStor);   
+         
     }
 
     @Override
     public void cleanup() {
     }
 }
-
-//
-//
-//
-// // Ueberpruefung der Speicherstaende nach Abflussereignissen und bei Bedarf
-//        // zuruecksetzen der Fuellstaende auf 0, falls Werte negativ sind
-//        if (soilStorage < 0) {
-//            soilStorage = 0;
-//        }
-//        if (depStorage < 0) {
-//            depStorage = 0;
-//        }
-
--- src/Snowmelt.java
+++ src/Snowmelt.java
@@ -16,7 +16,8 @@ import jams.model.*;
 @VersionComments(entries = {
     @VersionComments.Entry(version = "1.0_0", comment = "Initial version"),
     @VersionComments.Entry(version = "1.0_1", comment = "Some improvements")
-})        
+})    
+ 
 public class Snowmelt extends JAMSComponent {
 
     /*
@@ -73,44 +74,20 @@ public class Snowmelt extends JAMSComponent {
             upperBound = Double.POSITIVE_INFINITY
     )
     public Attribute.Double ddf; 
-     
-//    
-//    @JAMSVarDescription(
-//            access = JAMSVarDescription.AccessType.READ,
-//            description = //Aktueller Zeitschritt im Modell
-//    )
-//    public Attribute.?? time  //? Kalendervariable?
-// 
-//            
-//@JAMSVarDescription(
-//            access = JAMSVarDescription.AccessType.READ,
-//            description = "time interval, for which the model should be executed" //Zeitschritt und Zeitraum des Modells
-//    )
-//    public Attribute.?? timeInt //? 
- 
-        
-@JAMSVarDescription(
+
+
+    @JAMSVarDescription(
         access = JAMSVarDescription.AccessType.READ,
-        description = "Saturation value of SoilStorage. If value is exceeded, the "
-                        + "water flows into the depression storage",
+        description = "Saturation value of SoilStorage. If value is exceeded, "
+                       + "the water flows into the depression storage",
         lowerBound = 0,
         upperBound = Double.POSITIVE_INFINITY,
         unit = "mm"
-        )
-        public Attribute.Double satValue;    //Kalibrationsfaktor
+    )
+    public Attribute.Double satValue;   
         
 
-@JAMSVarDescription(
-            access = JAMSVarDescription.AccessType.WRITE,
-            description = "Potential amount of snowmelt/ snowmelt rate",
-            unit = "mm/d",
-            lowerBound = Double.NEGATIVE_INFINITY,
-            upperBound = Double.POSITIVE_INFINITY
-    )
-    public Attribute.Double snowMelt;
-
-
-@JAMSVarDescription(
+    @JAMSVarDescription(
         access = JAMSVarDescription.AccessType.READWRITE,
         description = "Filling of the snow storage after time step",
         lowerBound = 0,
@@ -120,27 +97,37 @@ public class Snowmelt extends JAMSComponent {
     public Attribute.Double snowStor;  
 
 
-@JAMSVarDescription(
+    @JAMSVarDescription(
         access = JAMSVarDescription.AccessType.READWRITE,
         description = "Filling of the soil storage after time step",
         lowerBound = 0,
         upperBound = Double.POSITIVE_INFINITY,
         unit = "mm"
-        )
-        public Attribute.Double soilStor; 
+    )
+    public Attribute.Double soilStor; 
 
 
-@JAMSVarDescription(
+    @JAMSVarDescription(
         access = JAMSVarDescription.AccessType.READWRITE,
         description = "Filling of the depression storage after time step",
         lowerBound = 0,
         upperBound = Double.POSITIVE_INFINITY,
         unit = "mm"
-        )
-        public Attribute.Double depStor;  
-        
-        
-@JAMSVarDescription(
+    )
+    public Attribute.Double depStor;        
+    
+    
+    @JAMSVarDescription(
+            access = JAMSVarDescription.AccessType.WRITE,
+            description = "Potential amount of snowmelt/ snowmelt rate",
+            unit = "mm/d",
+            lowerBound = Double.NEGATIVE_INFINITY,
+            upperBound = Double.POSITIVE_INFINITY
+    )
+    public Attribute.Double snowMelt;
+
+    
+    @JAMSVarDescription(
         access = JAMSVarDescription.AccessType.WRITE,
         description = "Effective amount of snowmelt/ snowmelt rate",
         lowerBound = 0,
@@ -149,71 +136,30 @@ public class Snowmelt extends JAMSComponent {
     )
     public Attribute.Double eff_snowMelt;
  
-
-//    private double timeunit; ? //Variable zum Speichern des Zeitintervalls im Modell ?
  
-   /*
+    /*
      *  Component run stages
      */
     
-      
     @Override
-    public void init() {
-       
-//        timeunit = this.timeInt.getTimeUnit;          //Zeitintervall (täglich oder monatlich), brauch ich das überhaupt?
-//        
-//        //Check if needed values are existing + set initial storages before first model run
-//        if (this.initSnowStor == null)
-//            getModel().getRuntime().sendHalt(JAMS.i18n("parameter initSnowStor unspecified"));
-//        else
-//            snowStor.setValue(initSnowStor.getValue());
-//        
-//        if (this.initSoilStor == null)   ?//raus?
-//            getModel().getRuntime().sendHalt(JAMS.i18n("parameter_Precipitation_initSoilStor_unspecified"));
-//        else
-//            soilStor.setValue(initSoilStor.getValue());
-//        
-//        if (this.initDepStor == null)   ?//raus?
-//            getModel().getRuntime().sendHalt(JAMS.i18n("parameter_Precipitation_initDepStor_unspecified"));
-//        else
-//            depStor.setValue(initDepStor.getValue());
-//        
-//        if (this.satValue == null)  ?//raus?
-//            getModel().getRuntime().sendHalt(JAMS.i18n("parameter_Precipitation_satValue_unspecified"));
+    public void init() { 
      }
  
     @Override
     public void run() {
-        
-//        // if there are no values for a time step            
-//        if (this.snowMelt == null)
-//            getModel().getRuntime().sendHalt(JAMS.i18n("parameter Snowmmelt_snowMelt unspecified"));
-//        
-        // Take given values
-        double bT = this.baseTemp.getValue();
-        double meanT = this.meantemp.getValue();
-        double maxT = this.maxtemp.getValue();
-        double DDF = this.ddf.getValue();
-        double meltT = this.meltTemp.getValue();
-        double averageT = 0.5 * (meanT + maxT); // daily average temperature (mean of meanT and maxT)
-        double snowmelt_amount;
-//        double snowStor = this.snowStor.getValue();
-//        double depStor = this.depStor.getValue(); ?//raus?
-//        double soilStor = this.soilStor.getValue(); ?//raus?
-//        double snowMelt = this.snowMelt.getValue();
-//        double sat = this.satValue.getValue(); ? //raus?
+                
+        double averagetemp = 0.5 * (meantemp.getValue() + maxtemp.getValue()); // daily average temperature (mean of meantemp and maxtemp)
         double snowMelt_eff;
          
         // If the average temperture is higher than the melting temperature, 
         // then the potential snowmelt amount can be calculated by using ddf, 
         // the base temperature and the average temperature
-        // http://www.tandfonline.com/doi/pdf/10.1080/02626668909491333
         
-        if (averageT > meltT){
-        snowmelt_amount = DDF *(averageT - bT);   
+        if (averagetemp > meltTemp.getValue()){
+        snowMelt.setValue(ddf.getValue() *(averagetemp - baseTemp.getValue()));   
         }
         else {
-        snowmelt_amount = 0;    
+        snowMelt.setValue(0);    
         }
         
                      
@@ -221,7 +167,7 @@ public class Snowmelt extends JAMSComponent {
                 
         if (snowMelt.getValue() <= snowStor.getValue()) { //amount of potential snow melt amount is less or equal than the snowStor
             snowMelt_eff = snowMelt.getValue(); //all of potential snow melt rate can effectively melt
-            snowStor.setValue(snowStor.getValue() - snowMelt.getValue()); //snowStor reduces to by the snowMelt
+            snowStor.setValue(snowStor.getValue() - snowMelt.getValue()); //snowStor reduces to by the snowMelt 
         } 
         else { //potential snow melt rate is higher than the snowStor
             snowMelt_eff = snowStor.getValue(); //only as much snow as stored can melt
@@ -240,20 +186,8 @@ public class Snowmelt extends JAMSComponent {
             depStor.setValue(depStor.getValue() + snowMelt_eff); 
             soilStor.setValue(satValue.getValue());  //soilStor gets saturation value
         }
-        
-//        
-//        //write values
-//        if Zeitintervall (tu) ist täglich { ?//wie definieren? brauch ich das überhaupt?
-//            snowMelt.setValue(snowmelt_amount); 
-//        }
-//        else if Zeitintervall (tu) ist monatlich {  ?//wie definieren?
-//            snowMelt.setValue(snowmelt_amount * this.time.getActualMaximum(Attribute.??)); ?? //tägliche Werte irgendwie summieren?
-//        }
-       
-//        this.snowStor.setValue(snowStor);
-          this.eff_snowMelt.setValue(snowMelt_eff);
-//        this.soilStor.setValue(soilStor); 
-//        this.depStor.setValue(depStor);   
+               
+          this.eff_snowMelt.setValue(snowMelt_eff); //write value
     }
 
     @Override
